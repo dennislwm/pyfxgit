2e86d166a57a1bb74062f15ec685562b31c0eb2a
Dennis Lee <dennislwm@yahoo.com.au>
2024-01-07 15:24:18 +0800

    Bump to 0.0.2 pandas==1.3.0

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ with open(path.join(strCwd, 'README.md'), encoding='utf-8') as f:
 # │ └──────────────────── minor version
 # └───────────────────────── major version
 setup(name='pyfxgit',
-  version='0.0.1',
+  version='0.0.2',
   description='This is a package of helper classes and libraries.',
   packages=['pyfxgit'],
-  install_requires=['matplotlib==3.2.2','mpl_finance','numpy==1.18.5','pandas==1.0.5'],
+  install_requires=['matplotlib==3.2.2','mpl_finance','numpy==1.18.5','pandas==1.3.0'],
   author='Dennis Lee',
   author_email='dennislwm@gmail.com',
   URL="https://github.com/dennislwm/pyfxgit", 
